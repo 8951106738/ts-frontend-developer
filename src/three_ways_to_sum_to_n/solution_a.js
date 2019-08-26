30c9847bf8990b98106f9e53f83a85947c2bae25
8951106738 <54520283+8951106738@users.noreply.github.com>
2019-08-26 08:53:10 +0530

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,14 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+var sum_to_n = function findsum(n) {
+   let result = 0;
+    for(let i =1; i<=n;i++)
+    {
+        result = result + i;
+    }
+    return result
+}
+let n = 5;
+console.log('sum of numbers from 1 to ${n} is $ {findsum(n)}'})
 };
